b493039821799cd3ad3a1d3493008e49733dff11
Robert Papp <papp.robert.s@gmail.com>
2016-03-19 14:36:37 +0100

    bumptech/glide#961 Add example that Downloads a lot of files via downloadOnly

---
+++ src/glide3/java/com/bumptech/glide/supportapp/github/_961_downloadonly/Downloader.java
@@ -0,0 +1,76 @@
+package com.bumptech.glide.supportapp.github._961_downloadonly;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.*;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.bumptech.glide.*;
+import com.bumptech.glide.request.FutureTarget;
+import com.bumptech.glide.request.target.Target;
+
+class Downloader extends AsyncTask<String, String, Downloader.Result> {
+	private static final String TAG = "Downloader";
+
+	private final RequestManager glide;
+	public Downloader(RequestManager glide) {
+		this.glide = glide;
+	}
+
+	@SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+	@Override protected Result doInBackground(String... params) {
+		@SuppressWarnings("unchecked")
+		FutureTarget<File>[] requests = new FutureTarget[params.length];
+		// fire everything into Glide queue
+		for (int i = 0; i < params.length; i++) {
+			if (isCancelled()) {
+				break;
+			}
+			requests[i] = glide
+					.load(params[i])
+					.downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+			;
+		}
+		// wait for each item
+		Result result = new Result();
+		for (int i = 0; i < params.length; i++) {
+			if (isCancelled()) {
+				for (int j = i; j < params.length; j++) {
+					if (requests[i] != null) {
+						Glide.clear(requests[i]);
+					}
+					result.failures.put(params[j], new CancellationException());
+				}
+				break;
+			}
+			try {
+				File file = requests[i].get(10, TimeUnit.SECONDS);
+				result.success.put(params[i], file);
+			} catch (Exception e) {
+				result.failures.put(params[i], e);
+			} finally {
+				Glide.clear(requests[i]);
+			}
+			publishProgress(params[i]);
+		}
+		return result;
+	}
+
+	@Override protected void onProgressUpdate(String... values) {
+		for (String url : values) {
+			Log.v(TAG, "Finished " + url);
+		}
+	}
+
+	@Override protected void onPostExecute(Result result) {
+		Log.i(TAG, String.format(Locale.ROOT, "Downloaded %d files, %d failed.",
+				result.success.size(), result.failures.size()));
+	}
+
+	static class Result {
+		final Map<String, File> success = new HashMap<>();
+		final Map<String, Exception> failures = new HashMap<>();
+	}
+}
---
+++ src/glide3/java/com/bumptech/glide/supportapp/github/_961_downloadonly/TestFragment.java
@@ -0,0 +1,31 @@
+package com.bumptech.glide.supportapp.github._961_downloadonly;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.supportapp.BaseFragment;
+
+public class TestFragment extends BaseFragment {
+	private Downloader downloader;
+	@Override public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		downloader = new Downloader(Glide.with(getContext().getApplicationContext()));
+		downloader.execute(
+				"http://placehold.it/100?text=1",
+				"http://placehold.it/100?text=2",
+				"http://placehold.it/100?text=3",
+				"http://placehold.it/100?text=4",
+				"xttp://placehold.it/100?text=5", // will fail
+				"http://placehold.it/100?text=6",
+				"http://placehold.it/100?text=7",
+				"http://placehold.it/100?text=8",
+				"http://placehold.it/100?text=9"
+		);
+	}
+
+	@Override public void onDestroy() {
+		downloader.cancel(true);
+		super.onDestroy();
+	}
+}

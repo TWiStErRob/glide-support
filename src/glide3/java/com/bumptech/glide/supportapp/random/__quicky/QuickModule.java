e4d928453820429f573f05bf6d1d3cfc0ac963e9
Robert Papp <papp.robert.s@gmail.com>
2016-03-21 14:48:26 +0100

    Better name for launcher icons

--- src/glide3/java/com/bumptech/glide/supportapp/random/__quicky/QuickModule.java
+++ src/glide3/java/com/bumptech/glide/supportapp/random/__quicky/QuickModule.java
@@ -8,6 +8,7 @@ import com.bumptech.glide.module.GlideModule;
 public class QuickModule implements GlideModule {
 	@Override public void applyOptions(Context context, GlideBuilder builder) {
 //		builder.setDiskCache(new ExternalCacheDiskCacheFactory(context));
+//		builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
 	}
 	@Override public void registerComponents(Context context, Glide glide) {
 //		glide.register(GlideUrl.class, InputStream.class, new HttpUrlGlideUrlLoader.Factory());

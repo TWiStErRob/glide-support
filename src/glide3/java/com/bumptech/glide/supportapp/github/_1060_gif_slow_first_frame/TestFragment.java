29bece26eeb2be478a077444f82a8576725b11d2
Robert Papp <papp.robert.s@gmail.com>
2016-03-25 15:32:41 +0100

    bumptech/glide#1060 slow first frame and GIF frame timings debug test

---
+++ src/glide3/java/com/bumptech/glide/supportapp/github/_1060_gif_slow_first_frame/TestFragment.java
@@ -0,0 +1,25 @@
+package com.bumptech.glide.supportapp.github._1060_gif_slow_first_frame;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.supportapp.*;
+import com.bumptech.glide.supportapp.utils.*;
+
+public class TestFragment extends GlideImageFragment {
+	@Override protected void load(Context context) throws Exception {
+		String url =
+//				"https://cloud.githubusercontent.com/assets/3822339/14021484/b8814c80-f201-11e5-93b0-b50076381286.gif";
+				"https://cloud.githubusercontent.com/assets/2906988/14031433/1bfc4106-f20d-11e5-94c6-861bbf3f4991.gif";
+		Glide
+				.with(context)
+				.load(url)
+				.diskCacheStrategy(DiskCacheStrategy.SOURCE)
+				.listener(new LoggingListener<String, GlideDrawable>())
+				.into(imageView)
+		;
+	}
+}

4acfd3dacb37c5dc7cc25619a4fbc0fdd2773c0d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-02-18 20:26:40 +0000

    bumptech/glide#1408 try to repro

---
+++ src/glide3/java/com/bumptech/glide/supportapp/github/_1408_gif_missing_parts/TestFragment.java
@@ -0,0 +1,21 @@
+package com.bumptech.glide.supportapp.github._1408_gif_missing_parts;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.*;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.supportapp.*;
+
+public class TestFragment extends GlideImageFragment {
+	@Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.github_1408, container, false);
+	}
+	
+	@Override protected void load(Context context) throws Exception {
+		Glide
+				.with(context)
+				.load("http://www.aikf.com/ask/resources/images/facialExpression/qq/9.gif")
+				.into(imageView);
+	}
+}

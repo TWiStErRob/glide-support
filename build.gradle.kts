a56eba7f35309e0e442f2480c5b904b8ff092b3f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-02-23 14:56:28 +0000

    Using the platform BOM correctly is enough

--- build.gradle.kts
+++ build.gradle.kts
@@ -186,20 +186,7 @@ fun DependencyHandler.glide4Implementation(dependencyNotation: Any): Dependency?
 	add("glide4Implementation", dependencyNotation)
 
 // Lock in the version of Kotlin used so that the transitive dependencies are consistently upgraded.
-dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-bom:1.8.10")
-}
 // https://kotlinlang.org/docs/whatsnew18.html#usage-of-the-latest-kotlin-stdlib-version-in-transitive-dependencies
-// Not using the kotlin-gradle-plugin, so doing it manually.
-configurations.configureEach {
-	resolutionStrategy.eachDependency {
-		if (requested.group == "org.jetbrains.kotlin" && requested.name == "kotlin-stdlib-jdk7") {
-			useTarget("${target.group}:kotlin-stdlib:${target.version}")
-			because("Since Kotlin 1.8, jdk7 and jdk8 have been merged into stdlib.")
-		}
-		if (requested.group == "org.jetbrains.kotlin" && requested.name == "kotlin-stdlib-jdk8") {
-			useTarget("${target.group}:kotlin-stdlib:${target.version}")
-			because("Since Kotlin 1.8, jdk7 and jdk8 have been merged into stdlib.")
-		}
-	}
+dependencies {
+	implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.10"))
 }

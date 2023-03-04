ac22691be02d7f72d541a5794bd706b82e5779c8
Róbert Papp <papp.robert.s@gmail.com>
2023-03-04 21:29:41 +0000

    Version Catalog based shadow builds (#47)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,17 @@ pluginManagement {
 		google()
 	}
 }
+
+dependencyResolutionManagement {
+	versionCatalogs {
+		create(defaultLibrariesExtensionName.get()) {
+			// Implicit behavior: from(files("gradle/libs.versions.toml"))
+			val shadowPath = settings.extra.get("com.bumptech.glide.build.shadow").toString()
+			if (shadowPath.isNotBlank()) {
+				// Overlay another toml file on top of the default one.
+				org.gradle.api.internal.catalog.parser.TomlCatalogFileParser
+					.parse(file(shadowPath).inputStream(), this)
+			}
+		}
+	}
+}

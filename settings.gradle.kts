6f3a7a8cefd1fdbb5897d308385d3b33ed3c5341
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-03-21 21:40:45 +0000

    Fix Gradle 8.1 import warnings in Android Studio

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import net.twisterrob.gradle.doNotNagAbout
+
 pluginManagement {
 	repositories {
 		mavenCentral()
@@ -18,3 +20,20 @@ dependencyResolutionManagement {
 		}
 	}
 }
+
+plugins {
+	id("net.twisterrob.gradle.plugin.settings") version "0.15.1"
+}
+
+val gradleVersion: String = GradleVersion.current().version
+
+// TODEL Gradle sync in IDEA 2023.1 (vs. Gradle 8.1): https://issuetracker.google.com/issues/274469173 
+@Suppress("MaxLineLength")
+doNotNagAbout(
+	"The org.gradle.util.VersionNumber type has been deprecated. " +
+		"This is scheduled to be removed in Gradle 9.0. " +
+		"Consult the upgrading guide for further information: " +
+		"https://docs.gradle.org/${gradleVersion}/userguide/upgrading_version_8.html#org_gradle_util_reports_deprecations",
+	// There are 3 instances on consecutive lines, so just ignore the whole file.
+	"at com.android.ide.gradle.model.builder.AndroidStudioToolingPluginKt.isGradleAtLeast(AndroidStudioToolingPlugin.kt:"
+)
